0a25a54f914682a358bc3a5466f401ccb66a3bb8
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-05-14 18:03:25 +0630

    update

--- includes/header.php
+++ includes/header.php
@@ -25,6 +25,16 @@ $userRole = $_SESSION['role'] ?? '';
             });
         }
     </script>
+    <!-- PushAlert Unified Code -->
+    <script type="text/javascript">
+        (function(d, t) {
+            var g = d.createElement(t),
+            s = d.getElementsByTagName(t)[0];
+            g.src = "https://cdn.pushalert.co/unified_52c16538b33eca8504cc06b26bf34a5b.js";
+            s.parentNode.insertBefore(g, s);
+        }(document, "script"));
+    </script>
+    <!-- End PushAlert Unified Code -->
 
     <!-- Dynamically injecting the separated CSS file -->
     <?php require_once __DIR__ . '/styles.php'; ?>
